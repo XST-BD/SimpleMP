b9e8e535d6a58168d7f5c88a1fd44fdaef214b7a
SM Sadat <smsadat788@gmail.com>
2025-11-30 18:55:20 +0600

    feat: Implement basic media processing w/pyav

---
+++ avcore/simplempcore.py
@@ -0,0 +1,24 @@
+import av
+import av.datasets
+
+input = av.open(av.datasets.curated("/home/pancake/Downloads/NRS.mp4"))
+output = av.open("/home/pancake/Projects/simplemp/dump/test.mkv", "w") 
+
+stream_map = {}
+
+for istream in input.streams:
+    ostream = output.add_stream_from_template(istream)
+    stream_map[istream.index] = ostream
+
+
+for packet in input.demux():
+    
+    # skip flushing packets that demuxer generates
+    if packet.dts is None:
+        continue
+
+    packet.stream = stream_map[packet.stream.index]
+    output.mux(packet)
+
+input.close()
+output.close()
